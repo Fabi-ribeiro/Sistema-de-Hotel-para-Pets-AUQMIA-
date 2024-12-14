92aced159b7f15a3b6ae903ac48ceee52566f22c
Fabi-ribeiro <128069775+Fabi-ribeiro@users.noreply.github.com>
2024-12-14 18:32:31 -0300

    ajuste class
    ajuste

--- Cadastro-cuidador-funcionario/src/main/java/utfpr/edu/br/Cadastro_cuidador/funcionario/service/FuncionarioService.java
+++ Cadastro-cuidador-funcionario/src/main/java/utfpr/edu/br/Cadastro_cuidador/funcionario/service/FuncionarioService.java
@@ -1,12 +1,12 @@
 package utfpr.edu.br.Cadastro_cuidador.funcionario.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import utfpr.edu.br.Cadastro_cuidador.funcionario.model.Funcionario;
 import utfpr.edu.br.Cadastro_cuidador.funcionario.repository.FuncionarioRepository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class FuncionarioService {
@@ -18,8 +18,9 @@ public class FuncionarioService {
         return funcionarioRepository.findAll();
     }
 
-    public Optional<Funcionario> buscarPorId(Long id) {
-        return funcionarioRepository.findById(id);
+    public Funcionario buscarPorId(Long id) {
+        return funcionarioRepository.findById(id)
+          .orElseThrow(() -> new RuntimeException("Funcionário não encontrado"));
     }
 
     public Funcionario salvarFuncionario(Funcionario funcionario) {
